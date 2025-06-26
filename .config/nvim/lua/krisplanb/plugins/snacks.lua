c091ccf3a2f17dd8ca42ea38dd1c4c48a14f7c9d
Krisplanb <kristoffer@planbit.no>
2025-06-26 13:42:49 +0200

    Update snacks.lua
    instead of desktop address -> local assets file

--- .config/nvim/lua/krisplanb/plugins/snacks.lua
+++ .config/nvim/lua/krisplanb/plugins/snacks.lua
@@ -127,7 +127,20 @@ return {
                     { section = "startup" },
                     {
                         section = "terminal",
-                        cmd = "ascii-image-converter ~/Desktop/Others/profiles.JPG -C -c",
+                        cmd = function ()
+                            local handle = io.popen("find ~/.config/nvim/assets -type f \\(-iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg'\\) | shuf -n 1")
+                            if handle then
+                                local result = handle:read("*a"):gsub("%s+$", "")
+                                handle:close()
+                                if result ~= "" then
+                                    return "ascii-image-converter " .. result .. " -C -c"
+                                else
+                                    return "echo 'No images found'"
+                                end
+                            end
+                            return nil
+                        end,
+
                         random = 15,
                         pane = 2,
                         indent = 15,
